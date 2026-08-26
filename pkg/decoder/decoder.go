270443979d162a303d52f02c4b6b09b33219afca
mertso13 <mertsagiroglu2004@gmail.com>
2026-08-26 23:14:18 +0300

    Add decoder package for PNG/JPEG/GIF ingestion

---
+++ pkg/decoder/decoder.go
@@ -0,0 +1,33 @@
+package decoder
+
+import (
+	"fmt"
+	"image"
+	"io"
+	"os"
+
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+)
+
+func Decode(r io.Reader) (image.Image, error) {
+	decodedImage, _, err := image.Decode(r)
+	if err != nil {
+		return nil, fmt.Errorf("decoding image: %w", err)
+	}
+	return decodedImage, nil
+}
+
+func DecodeFile(path string) (image.Image, error) {
+	imageFile, err := os.OpenFile(path, os.O_RDONLY, 0)
+	if err != nil {
+		return nil, fmt.Errorf("reading image file: %w", err)
+	}
+
+	decodedImage, err := Decode(imageFile)
+
+	defer imageFile.Close()
+
+	return decodedImage, err
+}
