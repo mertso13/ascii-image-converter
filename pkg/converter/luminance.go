289edc8e1586b4f7cbf4103209c75a127b725183
mertso13 <mertsagiroglu2004@gmail.com>
2026-08-26 13:53:05 +0300

    Rename luminance locals to camelCase

--- pkg/converter/luminance.go
+++ pkg/converter/luminance.go
@@ -5,8 +5,8 @@ package converter
 //
 //	Y = 0.2126*R + 0.7152*G + 0.0722*B
 func Luminance(r, g, b uint8) uint8 {
-	r_float, g_float, b_float := float64(r), float64(g), float64(b)
-	Y := (0.2126 * r_float) + (0.7152 * g_float) + (0.0722 * b_float)
-	Y_int := uint8(Y)
-	return Y_int
+	rFloat, gFloat, bFloat := float64(r), float64(g), float64(b)
+	Y := (0.2126 * rFloat) + (0.7152 * gFloat) + (0.0722 * bFloat)
+	YInt := uint8(Y)
+	return YInt
 }
