8abca4fc0a15b4cd0206d07a3aaca777bf216a91
mertso13 <mertsagiroglu2004@gmail.com>
2026-08-30 22:14:42 +0300

    Add WebP and BMP image format decoders

--- pkg/decoder/decoder.go
+++ pkg/decoder/decoder.go
@@ -6,13 +6,13 @@ import (
 	"io"
 	"os"
 
+	_ "golang.org/x/image/bmp"
+	_ "golang.org/x/image/webp"
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
 )
 
-// Decode decodes an image from r. PNG, JPEG, and GIF are supported;
-// the format is detected from the bytes themselves.
 func Decode(r io.Reader) (image.Image, error) {
 	decodedImage, _, err := image.Decode(r)
 	if err != nil {
@@ -21,8 +21,6 @@ func Decode(r io.Reader) (image.Image, error) {
 	return decodedImage, nil
 }
 
-// DecodeFile opens path and decodes it. The file is closed before
-// DecodeFile returns, success or failure.
 func DecodeFile(path string) (image.Image, error) {
 	imageFile, err := os.OpenFile(path, os.O_RDONLY, 0)
 	if err != nil {
